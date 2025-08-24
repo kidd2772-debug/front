e38802a49ff14b7b20467f48b87f39d794ce0d15
Vlad X <kidd2772@gmail.com>
2025-08-24 18:48:21 +0300

    feat: implement full auth flow with pinia

--- app.vue
+++ app.vue
@@ -8,15 +8,21 @@
 
 <script setup lang="ts">
 import { useAuthStore } from '~/store/auth';
-
 const authStore = useAuthStore();
-
+const { $api } = useNuxtApp();
 onMounted(async () => {
   const token = localStorage.getItem('authToken');
 
   if (token && !authStore.isAuthenticated) {
-    console.log('Found token, trying to restore session...');
+    console.log('Found token in localStorage, trying to restore session...');
     authStore.setToken(token);
+    try {
+      const user = await $api('/users/me');
+      authStore.setUser(user);
+      console.log('Session restored successfully for:', user.nickname);
+    } catch (error) {
+      console.error('Failed to restore session:', error);
+    }
   }
 });
 </script>
