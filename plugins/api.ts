2691fed11c4978df68a1366538068dd5c751ce29
Vlad X <kidd2772@gmail.com>
2025-08-24 20:46:17 +0300

    Fix telegram bot name

--- plugins/api.ts
+++ plugins/api.ts
@@ -1,9 +1,11 @@
 import { useAuthStore } from "~/store/auth";
 
 export default defineNuxtPlugin((nuxtApp) => {
+    const config = useRuntimeConfig();
     const authStore = useAuthStore();
 
     const api = $fetch.create({
+        baseURL: config.public.apiBaseUrl,
         onRequest({ request, options }) {
             if (authStore.token) {
                 const headers = options.headers as HeadersInit;
