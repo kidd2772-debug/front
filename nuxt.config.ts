892bacb1bb9b1a8c0d4e5ebc0ca1ae2742665fdc
Vlad X <kidd2772@gmail.com>
2025-08-24 21:01:49 +0300

    Fix telegram bot name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.BASE_URL || 'http://localhost:8000/api',
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api',
     }
   },
 
